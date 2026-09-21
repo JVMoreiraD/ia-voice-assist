997203d0e8ac8a6f9cef8b656c48dd0baab5d60b
JVMoreiraD <jvitormoreirad@hotmail.com>
2026-09-21 19:50:48 -0300

    feat: package listen

--- src/listen/listen.py
+++ src/listen/listen.py
@@ -1,8 +1,9 @@
-from text_to_speech import text_to_speech
+from collections.abc import Callable
+
 import speech_recognition as sr
 
 
-class Listen:
+class ListenMic:
     def __init__(self):
         self.r = sr.Recognizer()
 
@@ -13,28 +14,28 @@ class Listen:
         with sr.Microphone() as source:
             audio = self.r.listen(source, timeout=15)
         try:
+            # pyrefly: ignore [missing-attribute]
             text = self.r.recognize_google(audio, language="pt-BR")
             print(f"Você disse: {text}")
             return text
         except sr.UnknownValueError:
-            text_to_speech("Não consegui entender o que você disse.")
             return None
         except sr.RequestError as e:
-            print(f"Erro ao solicitar resultados do serviço de reconhecimento de fala; {e}")
             return None
 
-    def listen_and_wait(self, action: callable):
+    def listen_and_wait(self, action: Callable):
         while True:
             r = self.r
             with sr.Microphone() as source:
-                # print("Diga algo...")
                 audio = r.listen(source)
 
             try:
+                # pyrefly: ignore [missing-attribute]
                 fala = r.recognize_google(audio, language="pt-BR")
                 if "bonsai" in fala.lower():
                     action()
             except sr.UnknownValueError:
                 print("Não consegui entender o que você disse.")
             except sr.RequestError as e:
+                # pyrefly: ignore [missing-attribute]
                 print(f"Erro ao solicitar resultados do serviço de reconhecimento de fala; {e}")
